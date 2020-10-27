945ce4ee3c0e818163e04e5c9d1c0a0ec285ded2
flyflyknife <small.fly.knife@gmail.com>
2020-10-28 02:04:34 +0800

    finish animation

--- static/lottery/index.ts
+++ static/lottery/index.ts
@@ -13,6 +13,11 @@ class GameBlock {
     public data: IUser
     public id: number
     public g: number
+
+    public targetX: number
+    public targetY: number
+
+
     constructor(x: number, y: number, vx: number, vy: number, bg: string, data: IUser) {
         this.x = x;
         this.y = y;
@@ -33,6 +38,141 @@ class GameBlock {
 interface IGameSettings {
     countOfItemInRunningAtAnyTime: number // 初始运动的个数
     newCountPeerSecond: number // 每秒新增的元素个数
+    pickCountList: Array<number>
+}
+
+
+class EndAnimation {
+    private fixedBlock: Map<number, GameBlock>
+    private canvas: HTMLCanvasElement
+    private itemWidth: number
+    private itemHeight: number
+    constructor(canvas: HTMLCanvasElement, fixedBlockMap: Map<number, GameBlock>, setting: { width: number, height: number }) {
+        this.itemWidth = setting.width
+        this.itemHeight = setting.height
+        this.fixedBlock = fixedBlockMap
+        this.canvas = canvas
+        this.calculateFixedBlockRunParms()
+        this.run()
+    }
+    private hasStopBlock: Array<GameBlock> = []
+    // 计算目标位置，设置目标地址和vx,vy
+    private calculateFixedBlockRunParms() {
+        // 按50FPS算
+        const fps = 50
+        // 动画总共运行3秒
+        const runTime = 1
+
+        // 每行摆10个
+        let columnCount = 10
+        // 每个头像间的间隔
+        let marginX = 10
+        let marginY = 10
+        let totalSize = this.fixedBlock.size
+        let addRow = totalSize % columnCount == 0 ? 0 : 1
+        let rowCount = Math.floor(totalSize / columnCount) + addRow
+
+
+        let startX = (this.canvas.width - (this.itemWidth + marginX) * columnCount) / 2
+        let startY = (this.canvas.height - (this.itemHeight + marginY) * rowCount) / 2
+
+
+        let i = 0
+
+        const keys = this.fixedBlock.keys()
+        for (let key of keys) {
+
+            let item = this.fixedBlock.get(key)
+
+            // 该元素位于第几行
+            let currentRowIndex = Math.floor(i / columnCount)
+            console.log(currentRowIndex)
+            // 设置元素 的目标位置
+            item.targetX = startX + i * (this.itemWidth + marginX)
+            item.targetY = startY + currentRowIndex * (this.itemWidth + marginX)
+
+            item.vx = (item.targetX - item.x) / (fps * runTime)
+            item.vy = (item.targetY - item.y) / (fps * runTime)
+            // this.fixedBlock.set(key, item)
+            console.log(item)
+            i++
+
+        }
+
+
+    }
+
+    private draw() {
+        const ctx = this.canvas.getContext("2d")
+        const keys = this.fixedBlock.keys()
+        for (let key of keys) {
+            let item = this.fixedBlock.get(key)
+            if (!item) {
+                continue
+            }
+            // 元素已到达目标地址
+            // 更新位置
+            item.y += item.vy
+            item.x += item.vx
+
+            if (item.vx > 0) {
+                if (item.x >= item.targetX) {
+                    item.x = item.targetX
+                    item.vx = 0
+                }
+            } else if (item.vx < 0) {
+                if (item.x <= item.targetX) {
+                    item.x = item.targetX
+                    item.vx = 0
+                }
+            }
+            if (item.vy > 0) {
+                if (item.y >= item.targetY) {
+                    item.y = item.targetY
+                    item.vy = 0
+                }
+            } else if (item.vy < 0) {
+                if (item.y <= item.targetY) {
+                    item.y = item.targetY
+                    item.vy = 0
+                }
+            }
+            if (item.vy == 0 && item.vx == 0) {
+                this.fixedBlock.delete(key)
+                this.hasStopBlock.push(item)
+            }
+
+            // drawItem
+            ctx.fillStyle = item.bg
+            ctx.beginPath();
+            ctx.arc(item.x + this.itemWidth / 2, item.y + this.itemHeight / 2, this.itemWidth / 2, 0, 2 * Math.PI)
+            ctx.closePath();
+            ctx.fill();
+
+        }
+        // 绘制已经停止的元素
+        this.hasStopBlock.forEach((item) => {
+            ctx.fillStyle = item.bg
+            ctx.beginPath();
+            ctx.arc(item.x + this.itemWidth / 2, item.y + this.itemHeight / 2, this.itemWidth / 2, 0, 2 * Math.PI)
+            ctx.closePath();
+            ctx.fill();
+        })
+    }
+    // 将选择的对象，排列规则
+    private run() {
+        const ctx = this.canvas.getContext("2d")
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.draw()
+        requestAnimationFrame(() => {
+            if (this.fixedBlock.size == 0) {
+                return
+            }
+            this.run()
+        })
+
+
+    }
 }
 
 class GameScreen {
@@ -44,12 +184,13 @@ class GameScreen {
     private canvas: HTMLCanvasElement
     private runningStore: Map<number, GameBlock> = new Map<number, GameBlock>()//  存储当前运动的元素
     private preparePool: Array<GameBlock> = []
+    private pickCountList: Array<number> = []
     constructor(canvas: HTMLCanvasElement, userList: Array<IUser>, setting: IGameSettings) {
         this.canvas = canvas
 
         this.countOfItemInRunningAtAnyTime = setting.countOfItemInRunningAtAnyTime
         this.newCountPeerSecond = setting.newCountPeerSecond
-
+        this.pickCountList = setting.pickCountList
         this.initUserPool(userList)
     }
     // 打乱数组 洗牌
@@ -99,11 +240,11 @@ class GameScreen {
     start() {
         this.pickInitRunItems()
         this.addNewItemAtTime = Date.now()
-        this.animal(Date.now())
+        this.animation(Date.now())
     }
     // 需要固定的元素
     private fixedBlock: Map<number, GameBlock> = new Map<number, GameBlock>()
-    stop(pickCountList: Array<number>): Array<Array<IUser>> {
+    stop(): Array<Array<IUser>> {
         this.stopped = true
         // 当前屏幕上的所有元素
         let listOnScreen: Array<GameBlock> = []
@@ -120,16 +261,34 @@ class GameScreen {
         const luckyUserList: Array<Array<IUser>> = []
 
         let start = 0
-        for (let i = 0; i < pickCountList.length; i++) {
-            let list = listOnScreen.slice(start, start + pickCountList[i])
+        for (let i = 0; i < this.pickCountList.length; i++) {
+            let list = listOnScreen.slice(start, start + this.pickCountList[i])
             let q: Array<IUser> = []
             list.forEach((item) => {
+                // 存储固定元素
+                this.fixedBlock.set(item.id, item)
+                // 从运动中删除固定元素
+                this.runningStore.delete(item.id)
+
                 q.push(item.data)
             })
             luckyUserList.push(q)
-            start = pickCountList[i]
+            start = this.pickCountList[i]
         }
         return luckyUserList
+    }
+    // 绘制固定的元素
+    private drawFixedItem() {
+        const ctx = this.canvas.getContext("2d")
+        const keys = this.fixedBlock.keys()
+        for (let key of keys) {
+            let item = this.fixedBlock.get(key)
+            ctx.fillStyle = item.bg
+            ctx.beginPath();
+            ctx.arc(item.x + this.itemWidth / 2, item.y + this.itemHeight / 2, this.itemWidth / 2, 0, 2 * Math.PI)
+            ctx.closePath();
+            ctx.fill();
+        }
     }
 
     // 计算每个元素位置，并将它画出来
@@ -139,6 +298,9 @@ class GameScreen {
         const keys = this.runningStore.keys()
         for (let key of keys) {
             let item = this.runningStore.get(key)
+            if (!item) {
+                continue
+            }
             // 挑选超出边界的元素，从用户池删除
             // x 轴超出,  因为不计算反弹，那么y只能朝下方超出边界
             if (item.x > this.canvas.width || item.x < 0 - this.itemWidth || item.y > this.canvas.height) {
@@ -163,9 +325,14 @@ class GameScreen {
             item.y += item.vy
             item.x += item.vx
             item.vy += item.g * runTimeInterval;
-            this.runningStore.set(item.id, item)
+            // this.runningStore.set(item.id, item)
         }
 
+        // 如果动画已经停止了，那么不再补充新元素
+        if (this.stopped) {
+            this.drawFixedItem()
+            return
+        }
 
         // 那么补充新元素
         if (Date.now() - this.addNewItemAtTime < 200 + Math.random() * 800) {
@@ -192,7 +359,8 @@ class GameScreen {
             needAddNewItem--
         }
     }
-    private animal(lastTime: number) {
+    // 运动中
+    private animation(lastTime: number) {
         //  this.removeOutScreenAndFillFullUserPool(runningStore)
         const ctx = this.canvas.getContext("2d")
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
@@ -202,12 +370,15 @@ class GameScreen {
 
         this.computeEveryItemPositionAndDrawIt(runTimeInterval)
         requestAnimationFrame(() => {
-            if (this.stopped) {
+            if (this.runningStore.size == 0) {
+                new EndAnimation(this.canvas, this.fixedBlock, { width: this.itemWidth, height: this.itemHeight })
                 return
             }
-            this.animal(now)
+            this.animation(now)
         })
     }
+
+
 }
 
 
@@ -240,7 +411,8 @@ async function main() {
     const screenDom = <HTMLCanvasElement>document.getElementById('screen');
     game = new GameScreen(screenDom, setting.userList, {
         countOfItemInRunningAtAnyTime: 30,
-        newCountPeerSecond: 15
+        newCountPeerSecond: 15,
+        pickCountList: [10]
     })
     game.start()
     // Setup 1 获取用户 和抽奖 配置
@@ -250,7 +422,7 @@ async function main() {
 }
 function stopGame() {
     if (game) {
-        let list = game.stop([10])
+        let list = game.stop()
         console.log(list)
     }
 }
