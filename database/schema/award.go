754c6c98de627471eb03a7f95da81b362bf155a6
flyflyknife <small.fly.knife@gmail.com>
2020-12-01 18:03:36 +0800

    tmp

--- database/schema/award.go
+++ database/schema/award.go
@@ -2,9 +2,11 @@ package schema
 
 // 奖品
 type Award struct {
-	ID    int64  `xorm:"'id' unique index pk" json:"id"`
-	Name  string `xorm:"'name'" json:"name"`
-	Image string `xorm:"'image'" json:"image"`
+	ID       int64  `xorm:"'id' unique index pk" json:"id"`
+	Name     string `xorm:"'name'" json:"name"`
+	Image    string `xorm:"'image'" json:"image"`
+	Amount   string `xorm:"'amount'" json:"amount"`
+	Category string `xorm:"'category'" json:"category"`
 }
 
 func (a Award) TableName() string {
